b2857db624bd41aaada4981114cc738769c4acdf
Jason Lefever <jlefever@ycp.edu>
2019-02-21 21:47:22 -0500

    Include initial parser

--- MyCCompiler/Program.cs
+++ MyCCompiler/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Antlr4.Runtime;
+using System.IO;
 
 namespace MyCCompiler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            CParser parser;
+
+            using (var fileStream = new StreamReader(args[0]))
+            {
+                var inputStream = new AntlrInputStream(fileStream);
+                var lexer = new CLexer(inputStream);
+                var tokenStream = new CommonTokenStream(lexer);
+                parser = new CParser(tokenStream);
+            }
+
+            parser.primaryExpression();
         }
     }
 }
