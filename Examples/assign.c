61c6d7bbf0885df080a88ca9f6398f99531a6eb4
Jason Lefever <jlefever@ycp.edu>
2019-03-12 23:58:10 -0400

    Updated examples

--- Examples/assign.c
+++ Examples/assign.c
@@ -1,4 +1,4 @@
-﻿int foo(void), voo(void);
+﻿int foo(void), voo(void), boo(void), doo(void);
 
 int bar(char *x, int *p)
 {
@@ -8,9 +8,16 @@ int q;
 
 int main()
 {
-	char;
-	const unsigned int a;		/* a is a const int */
-	const int *b;		/* b is a pointer to a const int */
-	int *const c;		/* c is a const pointer to int */
-	const int *const d; /* d is a const pointerto a const int */
+	{
+		int x;
+		{
+			int w;
+		}
+		{
+		}
+	}
+	const unsigned int a;
+	const int *b;
+	int *const c;
+	const int *const d;
 }
