55a410de72286382dc60db45469dedc97651fc96
Jason Lefever <jlefever@ycp.edu>
2019-03-18 01:19:02 -0400

    Added other examples

---
+++ Examples/add.c
@@ -0,0 +1,13 @@
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int x, y, z;
+    x = 1;
+    y = 2;
+    z = add(x, y);
+    printf("%d", z);
+}
---
+++ Examples/precedence.c
@@ -0,0 +1,7 @@
+void main()
+{
+    int x = 2 + 3 * 4;
+    printf("%d\n", x);
+    int y = 3 * 4 + 2;
+    printf("%d\n", y);
+}
