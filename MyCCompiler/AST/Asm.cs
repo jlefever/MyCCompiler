a92447e51e0f861c0b99f07737d897ab47bf7421
Jason Lefever <jlefever@ycp.edu>
2019-05-09 20:48:52 -0400

    Add initial AsmBuilder

---
+++ MyCCompiler/AST/Asm.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+
+namespace MyCCompiler.AST
+{
+    public enum RegisterKind
+    {
+        Eax,
+        Ebx,
+        Ecx,
+        Edx,
+        Esi,
+        Edi,
+        Esp,
+        Ebp
+    }
+
+    public static class RegisterKindExtensions
+    {
+        public static string GetName(this RegisterKind registerKind)
+        {
+            return RegisterKindMap[registerKind];
+        }
+
+        private static readonly IDictionary<RegisterKind, string> RegisterKindMap = new Dictionary<RegisterKind, string>
+        {
+            { RegisterKind.Eax, "eax" },
+            { RegisterKind.Ebx, "ebx" },
+            { RegisterKind.Ecx, "ecx" },
+            { RegisterKind.Edx, "edx" },
+            { RegisterKind.Esi, "esi" },
+            { RegisterKind.Edi, "edi" },
+            { RegisterKind.Esp, "esp" },
+            { RegisterKind.Ebp, "ebp" }
+        };
+    }
+
+    public interface IOperand { }
+
+    public interface IWritableOperand : IOperand { }
+
+    public class Memory : IWritableOperand
+    {
+        public RegisterKind RegisterKind { get; }
+        public int Offset { get; }
+
+        public Memory(RegisterKind registerKind, int offset)
+        {
+            RegisterKind = registerKind;
+            Offset = offset;
+        }
+
+        public override string ToString()
+        {
+            if (Offset == 0)
+            {
+                return $"%({RegisterKind.GetName()})";
+            }
+
+            return $"{Offset}(%{RegisterKind.GetName()})";
+        }
+    }
+
+    public class Register : IWritableOperand
+    {
+        public RegisterKind RegisterKind { get; }
+
+        public Register(RegisterKind registerKind)
+        {
+            RegisterKind = registerKind;
+        }
+
+        public override string ToString()
+        {
+            return "%" + RegisterKind.GetName();
+        }
+    }
+
+    public class IntegerConstant : IOperand
+    {
+        public int Integer { get; }
+
+        public IntegerConstant(int integer)
+        {
+            Integer = integer;
+        }
+
+        public override string ToString()
+        {
+            return "$" + Integer;
+        }
+    }
+
+    public class TextConstant : IOperand
+    {
+        public string Text { get; }
+
+        public TextConstant(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return "$" + Text;
+        }
+    }
+
+    public interface ILine { }
+
+    public class Push : ILine
+    {
+        public IOperand Operand { get; }
+
+        public Push(IOperand operand)
+        {
+            Operand = operand;
+        }
+
+        public override string ToString()
+        {
+            return $"pushl\t{Operand}";
+        }
+    }
+
+    public class Mov : ILine
+    {
+        public IOperand Source { get; }
+        public IWritableOperand Destination { get; }
+
+        public Mov(IOperand source, IWritableOperand destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
+
+        public override string ToString()
+        {
+            return $"movl\t{Source}, {Destination}";
+        }
+    }
+
+    public class Call : ILine
+    {
+        public string Text { get; }
+
+        public Call(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"call\t{Text}";
+        }
+    }
+
+    public class Globl : ILine
+    {
+        public string Text { get; }
+
+        public Globl(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $".globl {Text}";
+        }
+    }
+
+    public class Label : ILine
+    {
+        public string Text { get; }
+
+        public Label(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"{Text}:";
+        }
+    }
+
+    public class Leave : ILine
+    {
+        public override string ToString()
+        {
+            return "leave";
+        }
+    }
+
+    public class Ret : ILine
+    {
+        public override string ToString()
+        {
+            return "ret";
+        }
+    }
+}
---
+++ MyCCompiler/AST/AsmBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCCompiler.AST
+{
+    public class AsmBuilder
+    {
+        public void Visit(CompilationUnit node)
+        {
+            foreach (var external in node.Externals)
+            {
+                Visit(external);
+            }
+        }
+
+        public void Visit(IExternal node)
+        {
+            switch (node)
+            {
+                case FunctionDefinition n:
+                    Visit(n);
+                    break;
+                case Declaration n:
+                    break;
+            }
+        }
+
+        public void Visit(FunctionDefinition node)
+        {
+            var list = new LinkedList<ILine>();
+
+            // Create globl directive and label for function
+            var funcName = "_" + node.FunctionDeclarator.Identifier.Text;
+            list.AddLast(new Globl(funcName));
+            list.AddLast(new Label(funcName));
+
+            // Write preamble
+            // Save the old base pointer value
+            list.AddLast(new Push(new Register(RegisterKind.Ebp)));
+            list.AddLast(new Mov(new Register(RegisterKind.Esp), new Register(RegisterKind.Ebp)));
+            list.AddLast(new Call("___main"));
+
+            // Write body
+
+            // Write epilogue
+            list.AddLast(new Leave());
+            list.AddLast(new Ret());
+        }
+
+        private static RegisterKind[] _callerSaved = { RegisterKind.Eax, RegisterKind.Ecx, RegisterKind.Edx };
+        private static RegisterKind[] _calleeSaved = { RegisterKind.Ebx, RegisterKind.Edi, RegisterKind.Esi };
+
+
+    }
+}
