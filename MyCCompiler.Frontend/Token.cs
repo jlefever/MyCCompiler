76fe869026e24a704ad6e4c35390db7ca4a2f4b0
Jason Lefever <jlefever@ycp.edu>
2019-07-29 21:57:59 -0400

    Create Token and TokenKind types

---
+++ MyCCompiler.Frontend/Token.cs
@@ -0,0 +1,23 @@
+﻿namespace MyCCompiler.Frontend
+{
+    public class Token
+    {
+        public TokenKind Kind { get; }
+        public string Lexeme { get; }
+        public object Literal { get; }
+        public int Line { get; }
+
+        public Token(TokenKind kind, string lexeme, object literal, int line)
+        {
+            Kind = kind;
+            Lexeme = lexeme;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return Kind + " " + Lexeme + " " + Literal;
+        }
+    }
+}
---
+++ MyCCompiler.Frontend/TokenKind.cs
@@ -0,0 +1,35 @@
+﻿namespace MyCCompiler.Frontend
+{
+    public enum TokenKind
+    {
+        // Single-character tokens
+        LeftParen, RightParen, LeftBrace, RightBrace, LeftBracket,
+        RightBracket, Comma, Colon, Semicolon, Hook, Tilde,
+
+        // One, two, or three character tokens
+        Equal, EqualEqual,
+        Bang, BangEqual,
+        Greater, GreaterEqual, GreaterGreater, GreaterGreaterEqual,
+        Less, LessEqual, LessLess, LessLessEqual,
+        Star, StarEqual,
+        Slash, SlashEqual,
+        Percent, PercentEqual,
+        Plus, PlusEqual, PlusPlus,
+        Minus, MinusEqual, MinusMinus, Arrow, // AKA "->"
+        Amp, AmpEqual, AmpAmp,
+        Pipe, PipeEqual, PipePipe,
+        Caret, CaretEqual,
+        Dot, DotDotDot,
+        
+        // Literals
+        Identifier, String, Integer, Floating, Character,
+
+        // Keywords
+        Auto, Register, Static, Extern, Typedef, Void, Char, Short, Int, Long,
+        Float, Double, Signed, Unsigned, Struct, Union, Const, Volatile,
+        Sizeof, Enum, Case, Default, If, Else, Switch, While, Do, For, Goto,
+        Continue, Break, Return,
+
+        Eof
+    }
+}
